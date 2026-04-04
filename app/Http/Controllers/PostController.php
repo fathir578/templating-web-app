5ffa5841ff1accb52829fa5225e07a7753e2dcf9
fathir <fathirsyahputra559@gmail.com>
2026-04-04 22:43:28 +0700

    Add CRUD functionality for posts in PostController and define routes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
@@ -10,5 +12,35 @@ class PostController extends Controller
     {
         $posts = Post::all();
         return view('posts.index', compact('posts'));
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        Post::create([
+        'title' => $request->title,
+        'body' => $request->body,
+    ]);
+
+        return redirect('/posts');
+    }
+    public function edit($id)
+    {
+        $post = Post::find($id);
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $post = Post::find($id);
+        $post->update([
+            'title' => $request->title,
+            'body' => $request->body,
+        ]);
+
+        return redirect('/posts');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('/', [DashboardController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
